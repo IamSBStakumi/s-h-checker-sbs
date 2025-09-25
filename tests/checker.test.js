ea8ced88de3427823625433ee2d6ef2bdbe456dd
kuri884 <krc.kuribayashi@gmail.com>
2025-09-25 09:59:00 +0900

    バージョン一致フラグも検証対象に追加

--- tests/checker.test.js
+++ tests/checker.test.js
@@ -2,6 +2,20 @@ const test = require("node:test");
 const assert = require("node:assert/strict");
 const path = require("path");
 const analyzer = require("../analyzer/analyzeYarnLock");
+const logger = require("../console/logger");
+
+function checkMessage(compromised) {
+  for (const pkg of compromised) {
+    if (pkg.isMatchVersion) {
+      logger.danger(`Package: ${pkg.package}, Version: ${pkg.version}`);
+      logger.danger(`${pkg.message}`);
+    } else {
+      // 侵害されたパッケージはあるが、バージョンは一致しなかった時
+      logger.info(`Package: ${pkg.package}, Version: ${pkg.version}`);
+      logger.info(`${pkg.message}`);
+    }
+  }
+}
 
 test("analyzeYarnLock関数が侵害されたパッケージを検出可能", () => {
   const testFilePath = path.resolve(
@@ -9,12 +23,15 @@ test("analyzeYarnLock関数が侵害されたパッケージを検出可能", ()
     "fixtures",
     "compromised.yarn.lock"
   );
-  const compromised = analyzer(testFilePath);
+  const result = analyzer(testFilePath);
+  checkMessage(result);
 
-  assert(Array.isArray(compromised), "Result should be an array");
-  assert(compromised.length === 2, "Should find compromised packages");
-  assert(compromised[0].package === "@crowdstrike/commitlint");
-  assert(compromised[0].version === "8.1.1");
-  assert(compromised[1].package === "@ctrl/shared-torrent");
-  assert(compromised[1].version === "6.3.1");
+  assert(Array.isArray(result), "Result should be an array");
+  assert(result.length === 2, "Should find compromised packages");
+  assert(result[0].package === "@crowdstrike/commitlint");
+  assert(result[0].version === "8.1.1");
+  assert(result[0].isMatchVersion);
+  assert(result[1].package === "@ctrl/shared-torrent");
+  assert(result[1].version === "6.3.1");
+  assert(result[1].isMatchVersion);
 });
