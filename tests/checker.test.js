b50255c5c56f111c3a5ea315937e6b8c2fc845ef
kuri884 <krc.kuribayashi@gmail.com>
2025-09-25 10:16:17 +0900

    侵害されていないパッケージは検出しないことを確認するテスト

--- tests/checker.test.js
+++ tests/checker.test.js
@@ -5,15 +5,19 @@ const analyzer = require("../analyzer/analyzeYarnLock");
 const logger = require("../console/logger");
 
 function checkMessage(compromised) {
-  for (const pkg of compromised) {
-    if (pkg.isMatchVersion) {
-      logger.danger(`Package: ${pkg.package}, Version: ${pkg.version}`);
-      logger.danger(`${pkg.message}`);
-    } else {
-      // 侵害されたパッケージはあるが、バージョンは一致しなかった時
-      logger.info(`Package: ${pkg.package}, Version: ${pkg.version}`);
-      logger.info(`${pkg.message}`);
+  if (compromised.length > 0) {
+    for (const pkg of compromised) {
+      if (pkg.isMatchVersion) {
+        logger.danger(`Package: ${pkg.package}, Version: ${pkg.version}`);
+        logger.danger(`${pkg.message}`);
+      } else {
+        // 侵害されたパッケージはあるが、バージョンは一致しなかった時
+        logger.info(`Package: ${pkg.package}, Version: ${pkg.version}`);
+        logger.info(`${pkg.message}`);
+      }
     }
+  } else {
+    logger.success("No compromised packages found.");
   }
 }
 
@@ -24,7 +28,9 @@ test("analyzeYarnLock関数が侵害されたパッケージを検出可能", ()
     "compromised.yarn.lock"
   );
   const result = analyzer(testFilePath);
+  logger.info("侵害パッケージ検出テストのログ");
   checkMessage(result);
+  logger.log("\n\n");
 
   assert(Array.isArray(result), "Result should be an array");
   assert(result.length === 2, "Should find compromised packages");
@@ -34,4 +40,18 @@ test("analyzeYarnLock関数が侵害されたパッケージを検出可能", ()
   assert(result[1].package === "@ctrl/shared-torrent");
   assert(result[1].version === "6.3.1");
   assert(result[1].isMatchVersion);
+});
+
+test("analyzeYarnLock関数で侵害されていないパッケージは出力しない", () => {
+  const testFilePath = path.resolve(__dirname, "fixtures", "safe.yarn.lock");
+
+  const result = analyzer(testFilePath);
+  logger.info(
+    "侵害されていないパッケージは検出しないことを確認するテストのログ"
+  );
+  checkMessage(result);
+  logger.log("\n\n");
+
+  assert(Array.isArray(result), "Result should be an array");
+  assert(result.length === 0);
 });
